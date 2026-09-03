f6ca2d602d23ee3a0327e34b664be0a3b3a1020e
Jake Gaylor <jhgaylor@gmail.com>
2026-09-03 02:25:02 -0400

    chore: bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Managoat.Sandbox.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/managoat/managoat_sandbox"
 
   def project do
